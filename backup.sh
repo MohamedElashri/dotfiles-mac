f33ff6af65484fb4318a9bfa5495f73db977c88b
Mohamed Elashri <muhammadelashri@gmail.com>
2023-07-19 07:41:42 -0400

    add new files to dotfiles

--- backup.sh
+++ backup.sh
@@ -5,16 +5,18 @@ brew list --cask > "lists/cask.txt"
 
 # Backup .zshrc and *.zsh files and overwrite the existing files
 
-cp -f ~/.zshrc           configs/zsh/.zshrc
-cp -f ~/aliases.zsh      configs/zsh/aliases.zsh
-cp -f ~/plugins.zsh      configs/zsh/plugins.zsh
-cp -f ~/environment.zsh  configs/zsh/environment.zsh
-cp -f ~/config.zsh       configs/zsh/config.zsh
-cp -f ~/functions.zsh    configs/zsh/functions.zsh
-cp -f ~/shellhistory.zsh configs/zsh/shellhistory.zsh
+cp -f ~/.zshrc           	configs/zsh/.zshrc
+cp -f ~/aliases.zsh      	configs/zsh/aliases.zsh
+cp -f ~/plugins.zsh      	configs/zsh/plugins.zsh
+cp -f ~/environment.zsh  	configs/zsh/environment.zsh
+cp -f ~/config.zsh       	configs/zsh/config.zsh
+cp -f ~/functions.zsh    	configs/zsh/functions.zsh
+cp -f ~/shellhistory.zsh 	configs/zsh/shellhistory.zsh
+cp -f ~/.p10k.zsh		 	configs/zsh/.p10k.zsh
 
 # Backup git config file
-cp -f ~/.gitconfig configs/git/.gitconfig
+cp -f ~/.gitconfig 			configs/git/.gitconfig
+cp -f ~/.gitignore_global 	configs/git/.gitignore_global
 
 # Backup vscode settings file
 cp -f "$HOME/Library/Application Support/Code/User/settings.json" "configs/vscode/settings.json"
