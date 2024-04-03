f2078ffb9b1123ba2323ef5f36b88455220e7880
MohamedElashri <mail@elashri.com>
2024-04-03 09:04:47 +0200

    april update

--- configs/cli/hyper/hyper.js/.hyper.js
+++ configs/cli/hyper/hyper.js/.hyper.js
@@ -141,7 +141,7 @@ module.exports = {
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
-    localPlugins: [dracula-pro],
+    localPlugins: ["dracula-pro"],
     keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
